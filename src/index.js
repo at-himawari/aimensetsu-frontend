b17df5165432acbfd5c9f131f9ea392894ce0b37
himawari-aerobytes <himawari-aerobytes@users.noreply.github.com>
2024-09-04 15:40:18 +0900

    amplify configの追加

--- src/index.js
+++ src/index.js
@@ -5,6 +5,24 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { CookiesProvider } from "react-cookie";
 import { BrowserRouter } from 'react-router-dom';
+import { cognitoUserPoolsTokenProvider } from "aws-amplify/auth/cognito";
+import { CookieStorage } from "aws-amplify/utils";
+
+import { Amplify } from 'aws-amplify'
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      region: 'ap-northeast-1',
+      userPoolId: process.env.REACT_APP_COGNITO_USER_POOL_ID,
+      userPoolClientId: process.env.REACT_APP_COGNITO_CLIENT_ID,
+      loginWith: {
+        email: true
+      }
+    },
+  }
+});
+cognitoUserPoolsTokenProvider.setKeyValueStorage(new CookieStorage());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
